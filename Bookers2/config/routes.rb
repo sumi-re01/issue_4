383103e29189c9b3e9828b0c754ee8408caca00d
sumi-re01 <owob.viola@gmail.com>
2021-03-03 09:24:10 +0000

    [Add]routing

--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -4,7 +4,21 @@ Rails.application.routes.draw do
 
    devise_for :users
 
-   resources :users, only: [:show, :index, :update, :edit]
+   resources :users, only: [:show, :index, :update, :edit] do
+      member do
+         get :following, :followers
+      end
+   end
+
+   resources :relationships, only: [:create, :destroy]
+
    resources :books, only: [:show, :index, :create, :edit, :update, :destroy]
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   # resources :users do
+   #    member do
+   #       get :following, :followers
+   #    end
+   # end
+
  end
