f072ccb33b994b94386fe455e7045aa1e3628159
sumi-re01 <owob.viola@gmail.com>
2021-03-03 07:25:46 +0000

    [Add]User(follower_id)-Relation-User(followed_id)

--- Bookers2/app/models/user.rb
+++ Bookers2/app/models/user.rb
@@ -10,5 +10,13 @@ class User < ApplicationRecord
 # association Book
   has_many :books, dependent: :destroy
 
+# association Relation
+# active(フォローする)
+  has_many :active_relationships, class_name:"Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed #一覧の取得
+# passive(フォローされる)
+  has_many :passive_relationships, class_name:"Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower #一覧の取得
+
   attachment :profile_image
 end
