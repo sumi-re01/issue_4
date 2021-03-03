55556376a0f3b661b943772a46dee4fe27566eb4
sumi-re01 <owob.viola@gmail.com>
2021-03-03 16:14:24 +0000

    [Add]

--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
    resources :relationships, only: [:create, :destroy]
 
+
    resources :books, only: [:show, :index, :create, :edit, :update, :destroy]
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
