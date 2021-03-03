d3ac02975c33d331b9cc8d8c047e9c42164b2bff
sumi-re01 <owob.viola@gmail.com>
2021-03-03 07:03:06 +0000

    [New]応用課題4

---
+++ Bookers2/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+   root to: "homes#top"
+   get 'home/about' => "homes#about"
+
+   devise_for :users
+
+   resources :users, only: [:show, :index, :update, :edit]
+   resources :books, only: [:show, :index, :create, :edit, :update, :destroy]
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ end
