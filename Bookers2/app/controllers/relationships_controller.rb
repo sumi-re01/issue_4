6f5b950dfed6111a4b70c0827e07de85f8ba8db8
sumi-re01 <owob.viola@gmail.com>
2021-03-03 09:25:34 +0000

    [Add]Follow

--- Bookers2/app/controllers/relationships_controller.rb
+++ Bookers2/app/controllers/relationships_controller.rb
@@ -1,2 +1,14 @@
 class RelationshipsController < ApplicationController
+
+  def create
+    user = User.find(params[:format])
+    current_user.follow(user)
+    redirect_back(fallback_location: users_path)
+  end
+
+  def destroy
+    user = Relationship.where(follower_id:params[:id])
+    current_user.unfollow(user)
+    redirect_back(fallback_location: users_path)
+  end
 end
